e1afbae1690725ed5533342bc31323a4b5dfb20f
Gambhiro <gambhiro.bhikkhu.85@gmail.com>
2017-08-14 07:19:44 +0100

    remove quotation marks

--- lib/asciidoctor-epub3/converter.rb
+++ lib/asciidoctor-epub3/converter.rb
@@ -357,8 +357,8 @@ document.addEventListener('DOMContentLoaded', function(event, reader) {
     footer_tag = footer_content.empty? ? nil : %(
 <footer>~ #{footer_content * ' '}</footer>)
     content = (convert_content node).strip.
-      sub(OpenParagraphTagRx, '<p><span class="open-quote">“</span>').
-      sub(CloseParagraphTagRx, '<span class="close-quote">”</span></p>')
+      sub(OpenParagraphTagRx, '<p>').
+      sub(CloseParagraphTagRx, '</p>')
     %(<div#{id_attr}#{class_attr}>
 <blockquote>
 #{content}#{footer_tag}
